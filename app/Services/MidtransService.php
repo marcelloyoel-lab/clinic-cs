93777f9d78f0afd8e3cad8b993729510891bf454
Marcello Yoel <juankwee21@gmail.com>
2026-06-29 18:39:43 +0700

    feat(midtrans): Add midtrans configuration

---
+++ app/Services/MidtransService.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Services;
+
+use Midtrans\Config;
+
+class MidtransService
+{
+  // Configure SDK only ya disini
+    public function __construct()
+    {
+        Config::$serverKey = config('midtrans.server_key');
+        Config::$clientKey = config('midtrans.client_key');
+        Config::$isProduction = config('midtrans.is_production');
+        Config::$isSanitized = config('midtrans.is_sanitized');
+        Config::$is3ds = config('midtrans.is_3ds');
+    }
+}
